5d322c22e63a6a0fb976aa697a23764fa9e09830
Shubham Padir <134799070+Spbrand@users.noreply.github.com>
2023-09-19 08:53:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+setInterval(() => {
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotation = 30 * htime + mtime / 2;
+    mrotation = 6 * mtime;
+    srotation = 6 * stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+}, 1000)
+
+
+// setTimeout(function(){
+// console.log("play it after 2 seconds")
+// },2000);
+
+var b = document.querySelector("h1")
+console.log(b)
